bfb1b80595864841a72f473728724b11fbc5bdaa
AlNomanCSE <abdullahalnomancse@gmail.com>
2024-12-23 04:26:39 -0800

    skeleton ready for api intergration

--- src/app/pages/loanapplication/loanapplication.component.ts
+++ src/app/pages/loanapplication/loanapplication.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Application, Loan } from '../../models/application.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loanapplication',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loanapplication.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LoanapplicationComponent {
-
+  application: Application = new Application();
+  loan: Loan = new Loan();
+  addLoan() {
+    const strObj = JSON.stringify(this.loan);
+    const newObj = JSON.parse(strObj);
+    this.application.Loans.push(newObj);
+  }
 }
