f6864d0ca431a509deceaaee4947ed92259bce7e
AlNomanCSE <abdullahalnomancse@gmail.com>
2024-12-23 04:55:49 -0800

    POST request added

--- src/app/pages/loanapplication/loanapplication.component.ts
+++ src/app/pages/loanapplication/loanapplication.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Application, Loan } from '../../models/application.model';
+import {
+  Application,
+  ApplicationApiResponse,
+  Loan,
+} from '../../models/application.model';
 import { CommonModule } from '@angular/common';
+import { MasterxerviceService } from '../../service/masterxervice.service';
 
 @Component({
   selector: 'app-loanapplication',
@@ -12,9 +17,25 @@ import { CommonModule } from '@angular/common';
 export class LoanapplicationComponent {
   application: Application = new Application();
   loan: Loan = new Loan();
+  constructor(private masterService: MasterxerviceService) {}
+
   addLoan() {
     const strObj = JSON.stringify(this.loan);
     const newObj = JSON.parse(strObj);
     this.application.Loans.push(newObj);
+  }
+  onSubmit() {
+    this.masterService.addNewApplication(this.application).subscribe(
+      (result: ApplicationApiResponse) => {
+        if (result.Result) {
+          alert(result.Message);
+        } else {
+          alert('Application failed');
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
